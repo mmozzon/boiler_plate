c44147739041e34cf3c24b7e23d84759ddc0ef59
mmozzon <mmozzon@gmail.com>
2024-12-27 14:18:42 +0100

    fix tailwind

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import '../index.css'
 const Home: React.FC = () => {
     return (
      <h1 className="flex-grow">
-        Ceci est la page d'accueil
+        Ceci est la page d'accueil du Boiler plate
      </h1>
     )
 };
